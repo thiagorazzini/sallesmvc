b70a8fbda4d87ded75585650882ab714361e9f0a
thiagorazzini <razzini.thiago@gmail.com>
2023-06-16 22:43:09 -0300

    Simple Create form

--- SalesWebMvc/Models/Seller.cs
+++ SalesWebMvc/Models/Seller.cs
@@ -20,7 +20,7 @@ namespace SalesWebMvc.Models
             
         }
 
-        public Seller(int id, string name, string email, DateTime birthDate , double baseSalary, Department department)
+        public Seller(int id, string name, string email, DateTime birthDate ,double baseSalary, Department department)
         {
             Id = id;
             Name = name;
